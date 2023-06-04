ab9cdf3036067bc90f7def6b74543cfa1e4fb13c
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-04 10:13:48 -0700

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,22 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
+
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """routes to present page status"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'])
+def count():
+    """endpoint that retrieves the number of each objects by type"""
+    countDict = {}
+    for cls in classes:
+        countDict[cls] = storage.count(classes[cls])
+        return jsonify(countDict)
+

5df912307afe5f7ae4aa04fb15df7de0d1dde954
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-04 09:46:09 -0700

    add api base

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, ressources={r"/api/*": {"origins": "0.0.0.0"}})
 
-app.url.map.strict_slashes = False
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
@@ -33,4 +33,4 @@ if __name__ == '__main__':
         HBNB_API_PORT = 5000
     else:
         HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=Truei)
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
